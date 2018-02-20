3bfab42352e0421df9942895815d87e826ba3425
HusDev <hus.bs4dev@gmail.com>
2018-02-20 19:55:38 +0300

    location deprecated stop point

--- app/src/main/java/com/dapp/husbs/taxtax/MainActivity.java
+++ app/src/main/java/com/dapp/husbs/taxtax/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
+//import android.support.v7.app.AlertDialog;
+import android.app.AlertDialog;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -26,17 +28,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import dmax.dialog.SpotsDialog;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnSignIn,btnRegister ;
-    RelativeLayout rootlayout ;
+    Button btnSignIn, btnRegister;
+    RelativeLayout rootlayout;
 
-    FirebaseAuth auth ;
-    FirebaseDatabase db ;
-    DatabaseReference users ;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -49,26 +52,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        fonts
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                                        .setDefaultFontPath("fonts/Arkhip_font.ttf")
-                                        .setFontAttrId(R.attr.fontPath)
-                                        .build());
+                .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
         setContentView(R.layout.activity_main);
 
 //        init Firebase
-        auth = FirebaseAuth.getInstance() ;
-        db = FirebaseDatabase.getInstance() ;
-        users =  db.getReference("Users") ;
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
 
 
-        btnRegister =(Button) findViewById(R.id.btnRegister) ;
-        btnSignIn = (Button)  findViewById(R.id.btnSignIn) ;
-        rootlayout =(RelativeLayout) findViewById(R.id.rootLayout) ;
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        rootlayout = (RelativeLayout) findViewById(R.id.rootLayout);
 
 //        Event
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            showRegisterDialog() ;
+                showRegisterDialog();
             }
         });
         btnSignIn.setOnClickListener(new View.OnClickListener() {
@@ -81,27 +84,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void showLoginDialog() {
 
-        final AlertDialog.Builder dialog =  new AlertDialog.Builder(this) ;
-        dialog.setTitle("SIGN IN") ;
-        dialog.setMessage("Please use email to SIGN IN ") ;
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("SIGN IN");
+        dialog.setMessage("Please use email to SIGN IN ");
 
-        LayoutInflater inflater = LayoutInflater.from(this) ;
-        View login_layout = inflater.inflate(R.layout.layout_login, null ) ;
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View login_layout = inflater.inflate(R.layout.layout_login, null);
 
-        final MaterialEditText edtEmail =  login_layout.findViewById(R.id.editEmail) ;
-        final MaterialEditText edtPassword =  login_layout.findViewById(R.id.editPassword) ;
+        final MaterialEditText edtEmail = login_layout.findViewById(R.id.editEmail);
+        final MaterialEditText edtPassword = login_layout.findViewById(R.id.editPassword);
 
 
-        dialog.setView(login_layout) ;
+        dialog.setView(login_layout);
 
 //        set Button
-        dialog.setPositiveButton("Register", new DialogInterface.OnClickListener() {
+        dialog.setPositiveButton("SIGN IN", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
+//                Set disable button Sign In if is processing
+                btnSignIn.setEnabled(false);
 
-
-//                click Validation
+                //                click Validation
                 if (TextUtils.isEmpty(edtEmail.getText().toString())) {
                     Snackbar.make(rootlayout, "please enter email address", Snackbar.LENGTH_SHORT).show();
                     return;
@@ -117,22 +121,31 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                final String email =  edtEmail.getText().toString() ;
-                final String password =  edtPassword.getText().toString() ;
-//                Login
-                auth.signInWithEmailAndPassword(email,password)
+                final String email = edtEmail.getText().toString();
+                final String password = edtPassword.getText().toString();
+
+                final AlertDialog waitingDialog = new SpotsDialog(MainActivity.this);
+                waitingDialog.show();
+
+                //                Login
+                auth.signInWithEmailAndPassword(email, password)
                         .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                             @Override
                             public void onSuccess(AuthResult authResult) {
-                                Intent intent =  new Intent(MainActivity.this,Welcome.class);
+                                waitingDialog.dismiss();
+                                Intent intent = new Intent(MainActivity.this, Welcome.class);
                                 startActivity(intent);
                                 finish();
                             }
                         }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Snackbar.make(rootlayout, "Failed "+ e.getMessage(),Snackbar.LENGTH_SHORT).show();
+                        waitingDialog.dismiss();
 
+                        Snackbar.make(rootlayout, "Failed " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+
+//                        Active Button
+                        btnSignIn.setEnabled(true);
                     }
                 });
 
@@ -140,33 +153,35 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-         dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-             @Override
-             public void onClick(DialogInterface dialogInterface, int i) {
+        dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
-             }
-         });
+
+            }
+        });
 
 
-        dialog.show() ;
+        dialog.show();
     }
 
+
     private void showRegisterDialog() {
-        final AlertDialog.Builder dialog =  new AlertDialog.Builder(this) ;
-        dialog.setTitle("REGISTER") ;
-        dialog.setMessage("Please use email to register ") ;
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("REGISTER");
+        dialog.setMessage("Please use email to register ");
 
-        LayoutInflater inflater = LayoutInflater.from(this) ;
-        View Resgister_layout = inflater.inflate(R.layout.layout_register, null ) ;
-
-
-        final MaterialEditText edtEmail =  Resgister_layout.findViewById(R.id.editEmail);
-        final MaterialEditText edtPassword =  Resgister_layout.findViewById(R.id.editPassword) ;
-        final MaterialEditText edtName =  Resgister_layout.findViewById(R.id.editName) ;
-        final MaterialEditText edtPhone =   Resgister_layout.findViewById(R.id.editPhone) ;
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View Resgister_layout = inflater.inflate(R.layout.layout_register, null);
 
 
-        dialog.setView(Resgister_layout) ;
+        final MaterialEditText edtEmail = Resgister_layout.findViewById(R.id.editEmail);
+        final MaterialEditText edtPassword = Resgister_layout.findViewById(R.id.editPassword);
+        final MaterialEditText edtName = Resgister_layout.findViewById(R.id.editName);
+        final MaterialEditText edtPhone = Resgister_layout.findViewById(R.id.editPhone);
+
+
+        dialog.setView(Resgister_layout);
 
 //        set Button
         dialog.setPositiveButton("Register", new DialogInterface.OnClickListener() {
@@ -175,37 +190,36 @@ public class MainActivity extends AppCompatActivity {
                 dialogInterface.dismiss();
 
 
-
 //                click Validation
-                if (TextUtils.isEmpty(edtEmail.getText().toString())){
-                    Snackbar.make(rootlayout,"please enter email address", Snackbar.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+                    Snackbar.make(rootlayout, "please enter email address", Snackbar.LENGTH_SHORT).show();
                     return;
                 }
 
-                if (TextUtils.isEmpty( edtPhone.getText().toString())){
-                    Snackbar.make(rootlayout,"please enter phone number", Snackbar.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(edtPhone.getText().toString())) {
+                    Snackbar.make(rootlayout, "please enter phone number", Snackbar.LENGTH_SHORT).show();
                     return;
                 }
-                if (TextUtils.isEmpty(edtPassword.getText().toString())){
-                    Snackbar.make(rootlayout,"please enter password", Snackbar.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+                    Snackbar.make(rootlayout, "please enter password", Snackbar.LENGTH_SHORT).show();
                     return;
                 }
-                if (edtPassword.getText().toString().length() < 6 ){
-                    Snackbar.make(rootlayout,"password too short !!!", Snackbar.LENGTH_SHORT).show();
+                if (edtPassword.getText().toString().length() < 6) {
+                    Snackbar.make(rootlayout, "password too short !!!", Snackbar.LENGTH_SHORT).show();
                     return;
                 }
 
 //                Register new User
-                final String email =  edtEmail.getText().toString() ;
-                final String password =  edtPassword.getText().toString() ;
-                final String phone =  edtPhone.getText().toString() ;
-                final String name =  edtName.getText().toString() ;
+                final String email = edtEmail.getText().toString();
+                final String password = edtPassword.getText().toString();
+                final String phone = edtPhone.getText().toString();
+                final String name = edtName.getText().toString();
                 auth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
                         .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                             @Override
                             public void onSuccess(AuthResult authResult) {
 //                                    save user to bd
-                                User user =  new User() ;
+                                User user = new User();
                                 user.setEmail(email);
                                 user.setName(name);
                                 user.setPassword(password);
@@ -213,30 +227,30 @@ public class MainActivity extends AppCompatActivity {
 //                                Use email to key
                                 users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                         .setValue(user)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Snackbar.make(rootlayout,"Register Successfully  !!!", Snackbar.LENGTH_SHORT).show();
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(rootlayout, "Register Successfully  !!!", Snackbar.LENGTH_SHORT).show();
 
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Snackbar.make(rootlayout," Failed" +e.getMessage() , Snackbar.LENGTH_LONG).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Snackbar.make(rootlayout, " Failed" + e.getMessage(), Snackbar.LENGTH_LONG).show();
 
-                                    }
-                                });
+                                            }
+                                        });
 
                             }
                         })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Snackbar.make(rootlayout," Failed" +e.getMessage() , Snackbar.LENGTH_SHORT).show();
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootlayout, " Failed" + e.getMessage(), Snackbar.LENGTH_LONG).show();
 
-                    }
-                });
+                            }
+                        });
 
             }
         });
@@ -244,11 +258,11 @@ public class MainActivity extends AppCompatActivity {
         dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-            dialogInterface.dismiss();
+                dialogInterface.dismiss();
             }
-        }) ;
+        });
 
-        dialog.show() ;
+        dialog.show();
 
     }
 
--- app/src/main/java/com/dapp/husbs/taxtax/Welcome.java
+++ app/src/main/java/com/dapp/husbs/taxtax/Welcome.java
@@ -1,13 +1,146 @@
 package com.dapp.husbs.taxtax;
 
-import android.support.v7.app.AppCompatActivity;
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class Welcome extends AppCompatActivity {
+import com.firebase.geofire.GeoFire;
+import com.github.glomadrian.materialanimatedswitch.MaterialAnimatedSwitch;
+import com.github.glomadrian.materialanimatedswitch.MaterialAnimatedSwitchState;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+
+public class Welcome extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
+
+    private GoogleMap mMap;
+//    Play service
+    private static final int MY_PERMISSION_REQUEST_CODE  = 7000 ;
+    private static final int PLAY_SERVIC_RES_REQUEST = 7001;
+
+    private LocationRequest mlocationRequest ;
+    private GoogleApiClient mGoogleApiClient ;
+    private Location mLastLocation    ;
+    private static int UPDATE_INTERVAL = 5000 ;
+    private static int FATEST_INTERVAL =3000 ;
+    private static int DISOKACEMENT = 10;
+
+    DatabaseReference drivers ;
+    GeoFire geoFire ;
+    Marker mCurrent  ;
+    MaterialAnimatedSwitch location_switch;
+    SupportMapFragment mapFragment ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+         mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+//        init View
+        location_switch = (MaterialAnimatedSwitch) findViewById(R.id.location_switch);
+        location_switch.setOnCheckedChangeListener(new MaterialAnimatedSwitch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(boolean isOnline) {
+                if (isOnline){
+                    startLocationUpdate();
+                    displayLcation() ;
+                    Snackbar.make(mapFragment.getView(),"You Are Online",Snackbar.LENGTH_SHORT).show();
+
+                }else{
+                    stopLocationUpdate() ;
+                    mCurrent.remove();
+                    Snackbar.make(mapFragment.getView(),"You Are Offline",Snackbar.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+    private void stopLocationUpdate() {
+    }
+
+    private void displayLcation() {
+    }
+
+    private void startLocationUpdate() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+         return;
+        }
+//        Deprecated
+//        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,  Welcome.this) ;
+
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
